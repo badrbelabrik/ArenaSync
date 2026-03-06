d9e7dc711b26640b22e4c0b09cade1b1e7670977
badrbelabrik <badr.belabrik@gmail.com>
2026-03-06 15:29:30 +0000

    Add navigation with React Router to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 export function Navbar(){
+    const navigate = useNavigate();
     return (
         <div className="navbar">
-            <button className="navbar-btn"><i className="fa-solid fa-house"></i>Home</button>
+            <button onClick={() => navigate("/")} className="navbar-btn"><i className="fa-solid fa-house"></i>Home</button>
             <button className="navbar-btn"><i className="fa-regular fa-calendar"></i>Tournament</button>
             <button className="navbar-btn"><i className="fa-solid fa-user"></i>Profile</button>
         </div>
--- src/components/TournamentDetails.jsx
+++ src/components/TournamentDetails.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { tournamentDB } from '../data/tournamentDB'
+import {TournamentCard} from "./TournamentCard.jsx";
+
+
 export function TournamentDetails() {
     const {id} = useParams();
     const [tournament, setTournament] = useState(tournamentDB.find(item => item.id == id))
     return (
-        <div> this is the details page of the {tournament.title} that have id : {id}</div>
+        <div className="details">
+            <TournamentCard tournament={tournament} />
+        </div>
     )
 }
